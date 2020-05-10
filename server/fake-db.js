d3cf19acb0bf0d79688865750b5da028f3315873
iwag <iwasakiyuki0824@gmail.com>
2020-05-10 15:07:56 +0900

    Nodeサーバ構築

---
+++ server/fake-db.js
@@ -0,0 +1,84 @@
+const Product = require('./model/product')
+
+class FakeDb{
+
+    constructor() {
+        this.products = [
+            {
+                coverImage: "./assets/img/phone-cover.jpg",
+                name:'Phone XL',
+                price: 799,
+                description: 'A large',
+                heading1:'サンプルテキスト１',
+                heading2:'サンプルテキスト２',
+                heading3:'サンプルテキスト３',
+                headingText1:'サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト',
+                headingText2:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+                headingText3:'テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章',
+            },
+            {
+                coverImage: "./assets/img/phone-cover.jpg",        
+                name:'Phone mini',
+                price: 699,
+                description: 'B large',
+                heading1:'サンプルテキスト１',
+                heading2:'サンプルテキスト２',
+                heading3:'サンプルテキスト３',
+                headingText1:'サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト',
+                headingText2:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+                headingText3:'テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章',
+            },
+            {
+                coverImage: "./assets/img/phone-cover.jpg",
+                name:'Phone Standard',
+                price: 299,
+                description: 'C large',
+                heading1:'サンプルテキスト１',
+                heading2:'サンプルテキスト２',
+                heading3:'サンプルテキスト３',
+                headingText1:'サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト',
+                headingText2:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+                headingText3:'テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章',
+            },
+            {
+                coverImage: "./assets/img/phone-cover.jpg",
+                name:'Phone small',
+                price: 199,
+                description: 'D large',
+                heading1:'サンプルテキスト１',
+                heading2:'サンプルテキスト２',
+                heading3:'サンプルテキスト３',
+                headingText1:'サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト',
+                headingText2:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+                headingText3:'テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章',
+            }        
+
+        ]
+    }
+
+    async initDb()
+    {
+        await this.cleanDb()
+        this.pushProductsToDb()
+    }
+
+   async cleanDb()
+    {
+    await Product.deleteMany({})
+    }
+
+    pushProductsToDb(){
+        this.products.forEach(
+            (product) =>{
+                const newProduct = new Product(product) 
+                newProduct.save()
+            }
+        )
+    }
+
+    seeDb(){
+        this.pushProductsToDb()
+    }
+}
+
+module.exports = FakeDb
